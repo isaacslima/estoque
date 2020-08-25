61a431e731ae00b958dbe32c22ae78a3e473f543
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-25 17:38:55 -0300

    add Domain

---
+++ Domain/Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entity
+{
+    public class BaseEntity
+    {
+        /// <summary>
+        /// Id (Identity)
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
---
+++ Domain/Entity/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entity
+{
+    public class Product : BaseEntity
+    {
+
+        public string Name { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public decimal UnitValue { get; set; }
+
+        public decimal TotalValue => Quantity * UnitValue;
+    }
+}
