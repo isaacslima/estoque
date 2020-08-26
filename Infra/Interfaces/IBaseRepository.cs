8e16aa3f40e90d09ba792db2670e70735fb25a1e
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 20:54:11 -0300

    create interface base repository

---
+++ Infra/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace Infra.Interfaces
+{
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : BaseEntity
+    {
+        bool Insert(TEntity entity);
+
+        bool Delete(TEntity entity);
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        bool Update(TEntity entity);
+    }
+}
