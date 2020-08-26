be1503e4c92fa0f1de53052a8ed35e744c6aeae8
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 20:48:44 -0300

    creating productMapper to use automap from DapperExtensions

---
+++ Infra/Mapping/ProductMapper.cs
@@ -0,0 +1,20 @@
+﻿using DapperExtensions.Mapper;
+using Domain.Entity;
+
+namespace Infra.Mapping
+{
+    public class ProductMapper : ClassMapper<Product>
+    {
+        public ProductMapper()
+        {
+            Table(nameof(Product));
+
+            Map(p => p.Id).Key(KeyType.Identity).Column("Id");
+            Map(p => p.Name).Column("Name");
+            Map(p => p.Quantity).Column("Quantity");
+            Map(p => p.UnitValue).Column("UnitValue");
+
+            AutoMap();
+        }
+    }
+}
