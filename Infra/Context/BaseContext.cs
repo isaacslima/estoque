860a904287d3ae1882d66dd1d40dd35dde5a6cd5
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 20:48:10 -0300

    creating base context for SQLite

---
+++ Infra/Context/BaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Infra.Config;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Infra.Context
+{
+    public class BaseContext : DbContext
+    {
+        public IDbConnection Connection { get; set; }
+
+        public  BaseContext()
+        {
+            Connection = new SqliteConnection("Filename=Database.db");
+            Database.Migrate();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=Database.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
