12628c8023a0c77e836aeb0286a75b92be268585
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 20:56:39 -0300

    adding controller

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Entity;
+using Infra.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Api.Controllers
 {
@@ -7,37 +12,107 @@ namespace Api.Controllers
     [Route("[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly ILogger<ProductController> _logger;
+        private IBaseRepository<Product> _productRepository;
 
-        public ProductController(ILogger<ProductController> logger)
+        public ProductController( IBaseRepository<Product> productRepository)
         {
-            _logger = logger;
+            _productRepository = productRepository;
         }
 
         [HttpGet]
-        public Product Get(int IdProduct)
+        [Route("{idProduct}")]
+        public ActionResult<Product> Get(int IdProduct)
         {
-            // TODO get product by id
-            var product = new Product{
-                Name = "Headset",
-                Quantity = 1,
-                UnitValue = (decimal)189.90,
-            };
-            return product;
+            try
+            {
+                var product = _productRepository.GetById(IdProduct);
+                if (product != null)
+                {
+                    return Ok(product);
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll()
+        {
+            try
+            {
+                var products = _productRepository.GetAll();
+                if (products.Count() > 0)
+                {
+                    return Ok(products);
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
-        public bool Post(Product product)
+        public IActionResult Post([FromBody] Product product)
         {
-            // TODO insert new product
-            return true;
+            try
+            {
+                if (_productRepository.Insert(product))
+                {
+                    return Ok();
+                }
+                return BadRequest("Não foi possível inserir o produto");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpDelete]
-        public bool Delete(Product product)
+        [Route("{idProduct}")]
+        public IActionResult Delete(int idProduct)
         {
-            // Todo delete product
-            return true;
+            try
+            {
+                var product = _productRepository.GetById(idProduct);
+                if(product == null)
+                {
+                    return NotFound();
+                }
+
+                if (_productRepository.Delete(product))
+                {
+                    return Ok();
+                }
+                return BadRequest("Não foi possível excluir o produto");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("{idProduct}")]
+        public IActionResult Put([FromBody] Product product)
+        {
+            try
+            {
+                if (_productRepository.Update(product))
+                {
+                    return Ok();
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
