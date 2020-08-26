4a554ae8845cb72f27b9c3e9832cdba22e8c361e
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 20:44:00 -0300

    creating product config for entity framework

---
+++ Infra/Config/ProductConfig.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Config
+{
+    public class ProductConfig : BaseConfig<Product>
+    {
+        public override void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable(nameof(Product));
+            builder.Property(d => d.Name).HasMaxLength(100);
+            builder.Property(d => d.Quantity).IsUnicode(false);
+            builder.Property(d => d.UnitValue).IsUnicode(false);
+            base.Configure(builder);
+        }
+    }
+}
