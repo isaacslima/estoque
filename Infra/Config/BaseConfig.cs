7b0e9db223b1e637ed955782832dac6999967932
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 20:43:30 -0300

    creating base config to entityframework

---
+++ Infra/Config/BaseConfig.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Config
+{
+    public class BaseConfig<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+
+        /// <summary>
+        /// Config Base Mapping
+        /// </summary>
+        /// <param name="builder">builder EntityType</param>
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder
+                 .HasKey(c => c.Id);
+        }
+
+    }
+}
