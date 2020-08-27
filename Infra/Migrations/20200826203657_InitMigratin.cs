d62e109960d8196a95d88aba2091ba76efe4c99f
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 21:07:01 -0300

    adding migrations

---
+++ Infra/Migrations/20200826203657_InitMigratin.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infra.Migrations
+{
+    public partial class InitMigratin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "text", maxLength: 100, nullable: true),
+                    Quantity = table.Column<int>(type: "integer", unicode: false, nullable: false),
+                    UnitValue = table.Column<double>(type: "real", unicode: false, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
