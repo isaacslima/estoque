b729806aa522b8ee94e49254ef48382d48326fb5
Isaac de Sousa <isaacslima@yahoo.com.br>
2020-08-26 21:06:52 -0300

    adding api configurations

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Infra.BaseRepository;
+using Infra.Context;
+using Infra.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using DapperExtensions;
 
 namespace Api
 {
@@ -17,6 +14,11 @@ namespace Api
     {
         public Startup(IConfiguration configuration)
         {
+            using (var client = new BaseContext())
+            {
+                client.Database.EnsureCreated();
+            }
+
             Configuration = configuration;
         }
 
@@ -26,6 +28,10 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddEntityFrameworkSqlite().AddDbContext<BaseContext>();
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            DapperExtensions.DapperExtensions.SqlDialect = new DapperExtensions.Sql.SqliteDialect();
+            DapperExtensions.DapperAsyncExtensions.SqlDialect = new DapperExtensions.Sql.SqliteDialect();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,11 @@ namespace Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(x => x
+             .AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
